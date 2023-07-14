59118577a6afd007725966ea5b1d003ddf9f4dab
sam hockaday <samuel.hockaday@gmail.com>
2023-07-14 21:30:47 +0100

    Adding JS clock

--- script.js
+++ script.js
@@ -1,3 +1,45 @@
+let output = document.getElementById('output');
+let numberDate = document.getElementById('date');
+
+let d = new Date();
+let day = d.getDay();
+let ndate = d.toLocaleString();
+
+switch (day) {
+    case 0:
+        day = "Sunday";
+        break;
+    case 1:
+        day = "Monday";
+        break;
+    case 2:
+        day = "Tuesday";
+        break;
+    case 3:
+        day = "Wednesday";
+        break;
+    case 4:
+        day = "Thursday";
+    case 5:
+        day = "Friday";
+        break;
+    case 6:
+        day = "Saturday";
+
+}
+
+
+output.innerHTML = day;
+date.innerHTML = ndate;
+
+
+
+
+
+
+
+
+
 const answers = [{
     answer: ` I studied Philosophy at university and realized that I enjoy the process of trying to solve problems both big and small.`,
 
@@ -51,3 +93,6 @@ qbtn5.addEventListener("click", function () {
 qbtn6.addEventListener("click", function () {
     answer.innerHTML = answers[5].answer;
 });
+
+let githubLink = document.getElementById("github");
+githubLink.href = "https://github.com/samscupoftea";
