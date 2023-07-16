a4f9693a6fed8696287aac6529c2d18d3f6ee74b
sam hockaday <samuel.hockaday@gmail.com>
2023-07-16 20:19:06 +0100

    Updated notation to JS.

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-//Sam's Record Shop JS Below 
-
+// Define the musicData object with information about different artists and their albums
 let musicData = {
     "Jeff Buckley": {
         album: "Grace",
@@ -9,47 +8,57 @@ let musicData = {
     "Laura Marling": {
         album: "I Speak Because I Can",
         year: 2010,
-        whyLove: "This record came out just as I was starting my first year at University in Manchester. Laura Marling's songwriting and unqie style was just so inspiring and just what I need when trying to develop my own material."
+        whyLove: "This record came out just as I was starting my first year at University in Manchester. Laura Marling's songwriting and unique style was just so inspiring and just what I needed when trying to develop my own material."
     },
     "Talk Talk": {
         album: "The Colour of Spring",
         year: 1986,
-        whyLove: "Talk Talk are mostly known for their 80's pop hit 'It's My Life', but they are so much more than that. Mark Hollis and the band went on to make increasingly nuanced and beautiful music. As time went on, Mark Hollis would go on to produce less and less commercialised music. For me, The Colour of Spring embodies the perfect balance of the catchy pop affectation that they started with, but with more varied arrangements and deeper material."
+        whyLove: "Talk Talk are mostly known for their 80's pop hit 'It's My Life', but they are so much more than that. Mark Hollis and the band went on to make increasingly nuanced and beautiful music. As time went on, Mark Hollis would go on to produce less and less commercialized music. For me, The Colour of Spring embodies the perfect balance of the catchy pop affectation that they started with, but with more varied arrangements and deeper material."
     },
-
 };
 
+// Select the table body element
 let musicTable = document.querySelector('#musicTable tbody');
 
+// Iterate over each artist in the musicData object
 for (let artistTitle in musicData) {
     let album = musicData[artistTitle];
 
+    // Create a new table row
     let row = document.createElement('tr');
 
+    // Create a cell for the artist's name and append it to the row
     let artistCell = document.createElement('td');
     artistCell.textContent = artistTitle;
     row.appendChild(artistCell);
 
+    // Create a cell for the album name and append it to the row
     let albumCell = document.createElement('td');
     albumCell.textContent = album.album;
     row.appendChild(albumCell);
 
+    // Create a cell for the album's release year and append it to the row
     let yearCell = document.createElement('td');
     yearCell.textContent = album.year;
     row.appendChild(yearCell);
 
+    // Create a cell for the reason why the artist loves the album and append it to the row
     let whyLoveCell = document.createElement('td');
     whyLoveCell.textContent = album.whyLove;
     row.appendChild(whyLoveCell);
 
+    // Append the row to the table
     musicTable.appendChild(row);
 }
 
+// Select the musicForm element
 let musicForm = document.querySelector('#musicForm');
 
+// Add an event listener to the form's submit event
 musicForm.addEventListener('submit', function (event) {
     event.preventDefault(); // Prevent the default form submission
 
+    // Select the input fields
     let artistInput = document.querySelector('#artistInput');
     let albumInput = document.querySelector('#albumInput');
     let yearInput = document.querySelector('#yearInput');
@@ -81,13 +90,20 @@ musicForm.addEventListener('submit', function (event) {
 
 //Live date and time clock JS Code. 
 
+// Select the output and numberDate elements
 let output = document.getElementById('output');
 let numberDate = document.getElementById('date');
 
+// Create a new Date object
 let d = new Date();
+
+// Get the current day of the week
 let day = d.getDay();
+
+// Get the current date and time as a localized string
 let ndate = d.toLocaleString();
 
+// Convert the day number to the corresponding day name
 switch (day) {
     case 0:
         day = "Sunday";
@@ -103,44 +119,33 @@ switch (day) {
         break;
     case 4:
         day = "Thursday";
+        break;
     case 5:
         day = "Friday";
         break;
     case 6:
         day = "Saturday";
-
+        break;
 }
 
-
+// Set the day name as the content of the output element
 output.innerHTML = day;
+
+// Set the current date and time as the content of the numberDate element
 date.innerHTML = ndate;
 
 
 // Array and JS for my Why Software Development.
 
+// Define an array of answers for the question "Why Software Development?"
 const answers = [
     {
         answer: `As a musician and a Philosophy graduate, creativity and the pursuit of knowledge go hand in hand for me. Since joining my first FAC meet-up back in January, I have thoroughly enjoyed the process of learning and finding my own way to build projects and enhance my understanding. During my studies, I have witnessed the diverse and personal ways in which individuals express their personalities through their coding style and problem-solving approaches.`,
     },
-    {
-        answer: `Creativity holds immense importance to me, and while developing my coding skills, I have had the opportunity to not only express a part of myself but also to be inspired by the ideas of others. This has motivated me to deepen my knowledge and expand my skill set. I appreciate the flexible nature of working with computers and the potential for collaborative work to achieve greater goals.`,
-    },
-    {
-        answer: `In my late teens, I read a book called 'Zen & The Art Of Motorcycle Maintenance' which profoundly changed my approach to life and the challenges it presents. Since then, I have been drawn to topics and endeavors that initially intimidate me. Coding, in particular, has long been an intimidating field. Starting my learning journey in January, I can confidently say that it has been one of the most rewarding pursuits I have undertaken.`,
-    },
-    {
-        answer: `Through teaching over the years in various locations worldwide, I have developed a deep respect for the importance of learning and honing skills. Software development is an area I am eager to explore, as it provides opportunities to accomplish a wide range of tasks and projects.`,
-    },
-    {
-        answer: `I strongly dislike the feeling of stagnation. I thrive on continuous growth, adaptation, and evolution. The field of software development is vast, diverse, and constantly evolving, making it an ideal career choice for me. The ever-changing landscape of coding languages and technologies is where I believe I will find plenty of fulfillment in the years to come.`,
-    },
-    {
-        answer: `I have always had a profound passion for technology and strive to stay updated with the latest developments in the field. My love for tech solidified when I got my first PlayStation console in 1999. Since then, I have utilized these tools for music production, video editing, and now, coding to create exciting projects.`,
-    },
+
 ];
 
-
-
+// Select the question buttons and the answer element
 let qbtn1 = document.querySelector("#Qbtn1");
 let qbtn2 = document.querySelector("#Qbtn2");
 let qbtn3 = document.querySelector("#Qbtn3");
@@ -149,33 +154,15 @@ let qbtn5 = document.querySelector("#Qbtn5");
 let qbtn6 = document.querySelector("#Qbtn6");
 let answer = document.querySelector(".answer");
 
+// Add event listeners to each question button
 qbtn1.addEventListener("click", function () {
     answer.innerHTML = answers[0].answer;
 });
 
-qbtn2.addEventListener("click", function () {
-    answer.innerHTML = answers[1].answer;
-});
 
-qbtn3.addEventListener("click", function () {
-    answer.innerHTML = answers[2].answer;
-});
-
-qbtn4.addEventListener("click", function () {
-    answer.innerHTML = answers[3].answer;
-});
-
-qbtn5.addEventListener("click", function () {
-    answer.innerHTML = answers[4].answer;
-});
-
-qbtn6.addEventListener("click", function () {
-    answer.innerHTML = answers[5].answer;
-});
-
+// Set the href attribute of the githubLink element to the GitHub repository URL
 let githubLink = document.getElementById("github");
 githubLink.href = "https://github.com/samscupoftea";
-
 
 // Sam's record shop.
 
